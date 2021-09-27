7ef01c45ee1efde0581df28f9e0d070e34e72288
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-09-28 00:08:42 +0200

    finished P5Conversion script
    number string = 5
    number = 56

---
+++ P5Conversion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P5Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+
+            int numberString;
+
+            Convert.ToString(number);
+
+            numberString = number;
+            
+            Console.WriteLine("numberstring = " + numberString);
+            
+
+            numberString = (int) 56.3;
+            
+            number = Convert.ToInt32(numberString);
+            
+            Console.WriteLine("number = " + number);
+        }
+    }
+}
