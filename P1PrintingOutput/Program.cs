1c81a71f2865cdb892ef6ebf5c5d861e6180222e
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-09-27 22:37:00 +0200

    Finished P1 and P2. Started P3
    forgot to commit earlier

---
+++ P1PrintingOutput/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("HelloWorld");
+      Console.WriteLine("No, THIS IS PATRICK!!");
+      Console.WriteLine("Aww geez... sorry");
+    }
+  }
+}
--- P2Comments/Program.cs
+++ P2Comments/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
- 
-namespace HelloWorld
+
+// Written by Harry Heath
+// Date: 27/09/2021
+// Email: harry.heath@student.forsbergsskola.se
+
+// Control Output: Hello World!
+namespace P2Comments
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("No, this is Patrick!!!");
         }
     }
 }
