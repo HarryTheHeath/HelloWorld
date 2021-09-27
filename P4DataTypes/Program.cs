199624ba344c53f19b90027a7ba51eddd369b713
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-09-27 23:16:30 +0200

    Finished P4 Variables

---
+++ P4DataTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P4DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string outputString = "Hello World!";
+
+            float decimalValue = 0.3f;
+
+            bool isAwesome = true;
+
+            byte heyByte = 0xA7;
+            
+            Console.WriteLine(outputString);
+            Console.WriteLine(decimalValue);
+            Console.WriteLine(isAwesome);
+            Console.WriteLine(heyByte);
+        }
+    }
+}
