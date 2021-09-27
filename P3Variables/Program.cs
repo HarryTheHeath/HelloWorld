399fe59cae82615af8c6fa8eb31c012d706eb017
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-09-27 22:55:42 +0200

    finished P3Variables + added to it
    Experimented with the code to mix writing and variables together, plus added in some simple maths

--- P3Variables/Program.cs
+++ P3Variables/Program.cs
@@ -6,7 +6,19 @@ namespace P3Variables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x;
+            x = 5;
+
+            int y;
+            y = 7;
+
+            int z;
+            z = x + y;
+
+            Console.WriteLine("x = " +x);
+            Console.WriteLine("y = " +y);
+            Console.WriteLine("x + y = z");
+            Console.WriteLine("z = " +z);
         }
     }
 }
