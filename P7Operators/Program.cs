f8121cdc030f079a2aea6be2ae9e33e67322892d
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-09-28 01:20:36 +0200

    finished P7
    got right answer but couldn't get divide to work with a decimal point  ( y / 1.2 )

---
+++ P7Operators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P7Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = 7 + 5;
+            int x = w * 3;
+            int y = x - 6;
+            int z = y - 5; // should be "y / 1.2" 
+            int mod = z % 17;
+
+            Console.WriteLine(mod);
+        }
+    }
+}
