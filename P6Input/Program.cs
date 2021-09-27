9def956139b7df8636199f23584dbbd0f5a8bd75
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-09-28 00:27:38 +0200

    Finished P6 Input
    added some extra dialogue

---
+++ P6Input/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P6Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oh hai there... what's your name?");
+
+            string input = Console.ReadLine();
+            
+            Console.WriteLine("Oh wow, you're the first " + input + " that I've ever met! :)");
+        }
+    }
+}
